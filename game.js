0ee903066673a2b7add710dc42404b7d5ef4e517
Silvio C <78268044+Snimpo@users.noreply.github.com>
2024-04-10 22:29:56 -0500

    Update game.js
    Had a flash function implemented but decided to follow the course's instructions and implemented a timeout function myself to get the flash effect by switching css clases with jQuery.

--- game.js
+++ game.js
@@ -14,20 +14,16 @@ function savedPattern(color) {
 }
 
 //This fucntion allows us to setup a flashing effect.
-$.fn.flash = function (times, duration) {
-  var $element = this;
-  times = times || 3;
-  duration = duration || 200;
+function animatePress(currentColour){
 
-  for (var i = 0; i < times; i++) {
-    (function () {
-      setTimeout(function () {
-        $element.fadeOut(duration, function () {
-          $element.fadeIn(duration);
-        });
-      }, i * duration * 2 + 50);
-    })(i);
-  }
+    $("#"+currentColour).removeClass(currentColour).addClass(".pressed");
+    var prevColor = currentColour;
+   
+    setTimeout(()=> {
+        $("#"+currentColour).removeClass(".pressed").addClass(prevColor);
+     }
+     ,100);
+
 };
 
 function playSound(name) {
@@ -36,29 +32,29 @@ function playSound(name) {
       case "red":
         var crash = new Audio("sounds/red.mp3");
         crash.play();
-        $("#" + name).flash(1, 200);
+        animatePress(name);
         break;
       case "blue":
         var kick = new Audio("sounds/blue.mp3");
         kick.play();
-        $("#" + name).flash(1, 200);
+        animatePress(name);
         break;
       case "green":
         var snare = new Audio("sounds/green.mp3");
         snare.play();
-        $("#" + name).flash(1, 200);
+        animatePress(name);
         break;
       case "yellow":
         var tom1 = new Audio("sounds/yellow.mp3");
         tom1.play();
-        $("#" + name).flash(1, 200);
+        animatePress(name);
         break;
 
       default:
         console.log("error");
     }
-/** 
-    function nextSequence() {
+ 
+   /* function nextSequence() {
         var gamePattern = [];
     
         var randomNumber = Math.floor(Math.random() * 4);
@@ -68,12 +64,40 @@ function playSound(name) {
         var randomChosenColor = buttonColours[randomNumber];
     
         gamePattern.push(randomChosenColor);
+
+
+        switch (randomChosenColor) {
+            case "red":
+              var crash = new Audio("sounds/red.mp3");
+              crash.play();
+              $("#" + randomChosenColor).flash(1, 200);
+              break;
+            case "blue":
+              var kick = new Audio("sounds/blue.mp3");
+              kick.play();
+              $("#" + randomChosenColor).flash(1, 200);
+              break;
+            case "green":
+              var snare = new Audio("sounds/green.mp3");
+              snare.play();
+              $("#" + randomChosenColor).flash(1, 200);
+              break;
+            case "yellow":
+              var tom1 = new Audio("sounds/yellow.mp3");
+              tom1.play();
+              $("#" + randomChosenColor).flash(1, 200);
+              break;
+      
+            default:
+              console.log("error");
+          }
     
-        //return (buttonColours[randomNumber]);
+        
     
     //alert(randomNumber);
   }
-*/
+  nextSequence();
+/** */
 
 }
 //nextSequence();
