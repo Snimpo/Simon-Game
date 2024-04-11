9eec71214675691694520ff3b19e15d98d12af00
Silvio C <78268044+Snimpo@users.noreply.github.com>
2024-04-10 21:33:04 -0500

    Update game.js
    Added blinking effects to the buttons as well as their respective sounds on click.

--- game.js
+++ game.js
@@ -1,77 +1,79 @@
+var userClickedPattern = [];
 
+$(".btn").click(function () {
+  var userChosenColour = this.id;
 
-//This function will determine what the nex color on the patern will be. 
-document.getElementById("red").addEventListener("click", nextSequence);
+  playSound(userChosenColour);
+  
+  savedPattern(userChosenColour);
+});
 
-
-
-function nextSequence() {
-
-
-    var gamePattern =[];
-    
-
-    var randomNumber = Math.floor(Math.random() * 4);
-    
-
-    var buttonColours = ["red", "blue", "green", "yellow"];
-
-    var randomChosenColor = buttonColours[randomNumber];
-
-    gamePattern.push(randomChosenColor);
-
-    //return (buttonColours[randomNumber]);
-    
-    $('#'+randomChosenColor).flash(1, 200);
-    playSound(randomNumber);
-
-    
-    //alert(randomNumber);
+function savedPattern(color) {
+  userClickedPattern.push(color);
+  console.log(userClickedPattern);
 }
 
 //This fucntion allows us to setup a flashing effect.
-$.fn.flash = function(times, duration) {
-    var $element = this;
-    times = times || 3;
-    duration = duration || 200;
+$.fn.flash = function (times, duration) {
+  var $element = this;
+  times = times || 3;
+  duration = duration || 200;
 
-    for (var i = 0; i < times; i++) {
-        (function() {
-            setTimeout(function() {
-                $element.fadeOut(duration, function() {
-                    $element.fadeIn(duration);
-                });
-            }, i * duration * 2 + 50);
-        })(i);
-    }
+  for (var i = 0; i < times; i++) {
+    (function () {
+      setTimeout(function () {
+        $element.fadeOut(duration, function () {
+          $element.fadeIn(duration);
+        });
+      }, i * duration * 2 + 50);
+    })(i);
+  }
 };
 
-function playSound(n){
+function playSound(name) {
 
-    switch(n){
-        case 0:
-            var crash = new Audio("sounds/red.mp3");
-            crash.play();
-            break;
-        case 1:
-            var kick = new Audio("sounds/blue.mp3");
-            kick.play();
-            break;  
-        case 2:
-            var snare = new Audio("sounds/green.mp3");
-            snare.play();
-            break;
-        case 3:
-            var tom1 = new Audio("sounds/yellow.mp3");
-            tom1.play();
-            break;
+    switch (name) {
+      case "red":
+        var crash = new Audio("sounds/red.mp3");
+        crash.play();
+        $("#" + name).flash(1, 200);
+        break;
+      case "blue":
+        var kick = new Audio("sounds/blue.mp3");
+        kick.play();
+        $("#" + name).flash(1, 200);
+        break;
+      case "green":
+        var snare = new Audio("sounds/green.mp3");
+        snare.play();
+        $("#" + name).flash(1, 200);
+        break;
+      case "yellow":
+        var tom1 = new Audio("sounds/yellow.mp3");
+        tom1.play();
+        $("#" + name).flash(1, 200);
+        break;
 
-        default: console.log("error");
-
+      default:
+        console.log("error");
     }
-
+/** 
+    function nextSequence() {
+        var gamePattern = [];
     
+        var randomNumber = Math.floor(Math.random() * 4);
+    
+        var buttonColours = ["red", "blue", "green", "yellow"];
+    
+        var randomChosenColor = buttonColours[randomNumber];
+    
+        gamePattern.push(randomChosenColor);
+    
+        //return (buttonColours[randomNumber]);
+    
+    //alert(randomNumber);
+  }
+*/
+
 }
-
-
-nextSequence();
+//nextSequence();
